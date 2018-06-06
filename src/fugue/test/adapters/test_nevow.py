bde82bdc80cdffd6b9bfcf16b74a221a99ad8d5e
Jonathan Jacobs <jonathan@jsphere.com>
2018-06-06 17:09:12 +0200

    Tests for the Nevow resource adapter.

---
+++ src/fugue/test/adapters/test_nevow.py
@@ -0,0 +1,44 @@
+from testtools import TestCase
+from testtools.matchers import Contains, Equals, Is, MatchesListwise
+from testtools.twistedsupport import failed, succeeded
+
+from fugue.adapters.nevow import nevow_adapter_resource
+from fugue.interceptors import before
+from fugue.interceptors.nevow import NEVOW_REQUEST
+from fugue.test.interceptors.test_nevow import fakeNevowRequest
+
+
+class NevowAdapterResourceTests(TestCase):
+    """
+    Tests for `nevow_adapter_resource`.
+    """
+    def test_leaf(self):
+        """
+        The resource identifies as a leaf.
+        """
+        resource = nevow_adapter_resource()
+        self.assertThat(
+            resource.locateChild(None, None),
+            MatchesListwise([
+                Is(resource),
+                Equals(())]))
+
+    def test_nevow_request(self):
+        """
+        Rendering the resource returns a successful deferred and inserted a
+        `NEVOW_REQUEST` value into the context.
+        """
+        def _spy(res):
+            def _spy_inner(context):
+                res.append(context)
+                return context
+            return before(_spy_inner)
+
+        requests = []
+        resource = nevow_adapter_resource([_spy(requests)])
+        self.assertThat(
+            resource.renderHTTP(fakeNevowRequest()),
+            succeeded(Equals(b'')))
+        self.assertThat(
+            requests,
+            MatchesListwise([Contains(NEVOW_REQUEST)]))
