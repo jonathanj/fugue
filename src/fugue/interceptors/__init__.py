7882768d685baa82c99cb1e85247fdba60b86849
Jonathan Jacobs <jonathan@jsphere.com>
2018-06-08 12:25:31 +0200

    Provide more convenience imports and improve some docstrings.

--- src/fugue/interceptors/__init__.py
+++ src/fugue/interceptors/__init__.py
@@ -1,8 +1,16 @@
 from .basic import (
-    after, around, before, error_handler, handler, middleware, on_request,
-    on_response)
+    Interceptor,
+    after,
+    around,
+    before,
+    error_handler,
+    handler,
+    middleware,
+    on_request,
+    on_response,
+)
 
 
 __all__ = [
-    'after', 'before', 'error_handler', 'around', 'handler', 'middleware',
-    'on_request', 'on_response']
+    'Interceptor', 'after', 'before', 'error_handler', 'around', 'handler',
+    'middleware', 'on_request', 'on_response']
